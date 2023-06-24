c44f3c7cae1c275e5edf1cfcdd8c2ec30febfdae
robinh15 <128604237+robinh15@users.noreply.github.com>
2023-06-24 16:23:28 +0200

    Delete Duplicate White Box Tests

--- src/test/java/fh/aalen/video/whitebox/WhiteBoxTests1.java
+++ src/test/java/fh/aalen/video/whitebox/WhiteBoxTests1.java
@@ -29,62 +29,6 @@ public class WhiteBoxTests1 {
     }
 
     @Test
-    public void testPersonConstructor() {
-        Person person = new Person();
-        Assertions.assertNotNull(person);
-    }
-
-    @Test
-    public void testPersonConstructorWithParameters() {
-        Person person = new Person(1, "Doe", new Date());
-        Assertions.assertEquals(1, person.getId());
-        Assertions.assertEquals("Doe", person.getSurname());
-        Assertions.assertNotNull(person.getBirthdate());
-    }
-
-    @Test
-    public void testPersonGetBirthdate() {
-        Date birthdate = new Date();
-        person.setBirthdate(birthdate);
-
-        Date retrievedBirthdate = person.getBirthdate();
-        Assertions.assertEquals(birthdate, retrievedBirthdate);
-    }
-
-    @Test
-    public void testPersonSetSurname() {
-        String surname = "Smith";
-        person.setSurname(surname);
-
-        String retrievedSurname = person.getSurname();
-        Assertions.assertEquals(surname, retrievedSurname);
-    }
-
-    @Test
-    public void testPersonSetId() {
-        long id = 2;
-        person.setId(id);
-
-        long retrievedId = person.getId();
-        Assertions.assertEquals(id, retrievedId);
-    }
-
-    @Test
-    public void testVideoConstructor() {
-        Video video = new Video();
-        Assertions.assertNotNull(video);
-    }
-
-    @Test
-    public void testVideoConstructorWithParameters() {
-        Video video = new Video("Sample Video", "PG", "A sample video", "Action");
-        Assertions.assertEquals("Sample Video", video.getTitle());
-        Assertions.assertEquals("PG", video.getAgeRating());
-        Assertions.assertEquals("A sample video", video.getDescription());
-        Assertions.assertEquals("Action", video.getGenre());
-    }
-
-    @Test
     public void testVideoGetPersonFavorites() {
         List<Person> personFavorites = new ArrayList<>();
         personFavorites.add(person);
@@ -93,42 +37,6 @@ public class WhiteBoxTests1 {
 
         List<Person> retrievedFavorites = video.getPersonFavorites();
         Assertions.assertEquals(personFavorites, retrievedFavorites);
-    }
-
-    @Test
-    public void testVideoSetTitle() {
-        String title = "Updated Video";
-        video.setTitle(title);
-
-        String retrievedTitle = video.getTitle();
-        Assertions.assertEquals(title, retrievedTitle);
-    }
-
-    @Test
-    public void testVideoSetAgeRating() {
-        String ageRating = "R";
-        video.setAgeRating(ageRating);
-
-        String retrievedAgeRating = video.getAgeRating();
-        Assertions.assertEquals(ageRating, retrievedAgeRating);
-    }
-
-    @Test
-    public void testVideoSetDescription() {
-        String description = "An updated video";
-        video.setDescription(description);
-
-        String retrievedDescription = video.getDescription();
-        Assertions.assertEquals(description, retrievedDescription);
-    }
-
-    @Test
-    public void testVideoSetGenre() {
-        String genre = "Comedy";
-        video.setGenre(genre);
-
-        String retrievedGenre = video.getGenre();
-        Assertions.assertEquals(genre, retrievedGenre);
     }
 
     @Test
